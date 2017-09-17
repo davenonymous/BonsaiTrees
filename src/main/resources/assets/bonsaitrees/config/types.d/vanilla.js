cb433b9459729570d4819f538bdf1386212f789a
thraaawn <david.hilberath@gmx.de>
2017-09-17 22:43:24 +0200

    Fix leaves drops

--- src/main/resources/assets/bonsaitrees/config/types.d/vanilla.js
+++ src/main/resources/assets/bonsaitrees/config/types.d/vanilla.js
@@ -11,6 +11,8 @@ var main = function() {
         'darkoak': 5
     };
 
+    var leaveDropChance = 10;
+
     Object.keys(treeTypes).forEach(function(type) {
         var meta = treeTypes[type];
         var vanillaType = new TreeType("minecraft:sapling", meta);
@@ -19,19 +21,29 @@ var main = function() {
         vanillaType.addDrop("minecraft:planks", meta, 1, 25);
         vanillaType.addDrop("minecraft:log", meta, 1, 5);
         vanillaType.addDrop("minecraft:sapling", meta, 1, 5);
-        vanillaType.addDrop("minecraft:leaves", meta, 1, 10);
 
         if(type == "oak") {
             vanillaType.addDrop("minecraft:apple", 0, 1, 10);
+            vanillaType.addDrop("minecraft:leaves", meta, 1, leaveDropChance);
+        }
+
+        if(type == "birch" || type == "spruce") {
+            vanillaType.addDrop("minecraft:leaves", meta, 1, leaveDropChance);
+        }
+
+        if(type == "jungle") {
+            vanillaType.addDrop("minecraft:leaves", meta, 1, leaveDropChance);
         }
 
         if(type == "darkoak") {
             vanillaType.addDrop("minecraft:log", meta, 2, 50);
+            vanillaType.addDrop("minecraft:leaves2", 1, 1, leaveDropChance);
             vanillaType.setGrowTime(900);
         }
 
         if(type == "acacia") {
             vanillaType.setGrowTime(450);
+            vanillaType.addDrop("minecraft:leaves2", 0, 1, leaveDropChance);
         }
 
         TreeTypeRegistry.registerTreeType(type, vanillaType);
