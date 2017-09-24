c0c68553f56f44fe277538e654b6533c66b1d392
thraaawn <david.hilberath@gmx.de>
2017-09-24 13:53:14 +0200

    Initial ExtraTrees compatibility
    Just use some javascript magic to loop over all Forestry trees instead
    of hardcoding them. This basically means Forestry-based Trees only need
    shape .json files from now on.
    I will try to add a command that will generate those for you.

--- src/main/java/org/dave/bonsaitrees/trees/TreeTypeForestry.java
+++ src/main/java/org/dave/bonsaitrees/trees/TreeTypeForestry.java
@@ -4,10 +4,12 @@ import forestry.api.arboriculture.EnumGermlingType;
 import forestry.api.arboriculture.ITree;
 import forestry.api.arboriculture.TreeManager;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.dave.bonsaitrees.base.BaseTreeType;
+import org.dave.bonsaitrees.utility.Logz;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,21 +50,45 @@ public class TreeTypeForestry extends BaseTreeType {
         }
 
         ITree forestryTree = TreeManager.treeRoot.getMember(stack);
-        String speciesName = "forestry:" + forestryTree.getGenome().getPrimary().getUnlocalizedName().substring(18);
+        String modId = forestryTree.getGenome().getPrimary().getModID();
+        String treeId = forestryTree.getGenome().getPrimary().getUnlocalizedName();
+        if(modId.equals("forestry")) {
+            treeId = treeId.replace("for.trees.species.", "");
+        } else if(modId.equals("extratrees")) {
+            treeId = treeId.replace("extratrees.species.", "");
+            treeId = treeId.replace(".name", "");
+        }
+        treeId = forestryTree.getGenome().getPrimary().getModID() + ":" + treeId;
 
-        return typeName.equals(speciesName);
+        return typeName.equals(treeId);
     }
 
     @Override
     public ItemStack getExampleStack() {
         if(exampleStack == null || exampleStack.isEmpty()) {
             for(ITree forestryTree : TreeManager.treeRoot.getIndividualTemplates()) {
-                String speciesName = "forestry:" + forestryTree.getGenome().getPrimary().getUnlocalizedName().substring(18);
-                if(speciesName.equals(typeName)) {
+                String modId = forestryTree.getGenome().getPrimary().getModID();
+
+                String treeId = forestryTree.getGenome().getPrimary().getUnlocalizedName();
+                if(modId.equals("forestry")) {
+                    treeId = treeId.replace("for.trees.species.", "");
+                } else if(modId.equals("extratrees")) {
+                    treeId = treeId.replace("extratrees.species.", "");
+                    treeId = treeId.replace(".name", "");
+                }
+                treeId = forestryTree.getGenome().getPrimary().getModID() + ":" + treeId;
+
+                if(treeId.equals(typeName)) {
                     exampleStack = forestryTree.getGenome().getSpeciesRoot().getMemberStack(forestryTree, EnumGermlingType.SAPLING);
                 }
             }
         }
+
+        if(exampleStack == null) {
+            Logz.info("Could not find example stack for tree: %s", this.typeName);
+            exampleStack = new ItemStack(Blocks.SAPLING, 1, 0);
+        }
+
         return exampleStack;
     }
 }
--- src/main/resources/assets/bonsaitrees/config/types.d/forestry.js
+++ src/main/resources/assets/bonsaitrees/config/types.d/forestry.js
@@ -7,298 +7,55 @@ isEnabled = function() {
 
 var main = function() {
     var TreeTypeForestry = Java.type("org.dave.bonsaitrees.trees.TreeTypeForestry");
+    var TreeManager = Java.type("forestry.api.arboriculture.TreeManager");
 
     var defaultDrops = [
         { name: "minecraft:stick", meta: 0, amount: stickAmount, chance: stickChance }
     ];
 
-    var forestryTrees = {
-        appleOak: {
-            drops: defaultDrops.concat([
-                { name: "minecraft:planks", meta: 0, amount: plankAmount, chance: plankChance },
-                { name: "minecraft:log", meta: 0, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 0, amount: leafAmount, chance: leafChance },
-                { name: "minecraft:apple", meta: 0, amount: fruitAmount, chance: fruitChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        darkOak: {
-            drops: defaultDrops.concat([
-                { name: "minecraft:planks", meta: 5, amount: plankAmount, chance: plankChance },
-                { name: "minecraft:log2", meta: 1, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 1, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        silverBirch: {
-            drops: defaultDrops.concat([
-                { name: "minecraft:planks", meta: 2, amount: plankAmount, chance: plankChance },
-                { name: "minecraft:log", meta: 2, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 2, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        silverLime: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 3, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.0", meta: 3, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 3, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        commonWalnut: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 13, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.3", meta: 1, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 4, amount: leafAmount, chance: leafChance },
-                { name: "forestry:fruits", meta: 1, amount: fruitAmount, chance: 5 }
-            ]),
-            growTime: defaultGrowTime
-        },
-        sweetChestnut: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 4, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.1", meta: 0, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 5, amount: leafAmount, chance: leafChance },
-                { name: "forestry:fruits", meta: 2, amount: 1, chance: 5 }
-            ]),
-            growTime: defaultGrowTime
-        },
-        hillCherry: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 15, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.3", meta: 3, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 6, amount: leafAmount, chance: leafChance },
-                { name: "forestry:fruits", meta: 0, amount: fruitAmount, chance: fruitChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        lemon: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 7, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.5", meta: 3, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 7, amount: leafAmount, chance: leafChance },
-                { name: "forestry:fruits", meta: 3, amount: fruitAmount, chance: fruitChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        plum: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 5, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.5", meta: 1, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 8, amount: leafAmount, chance: leafChance },
-                { name: "forestry:fruits", meta: 4, amount: fruitAmount, chance: fruitChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        sugarMaple: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 6, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.5", meta: 2, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 9, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        redSpruce: {
-            drops: defaultDrops.concat([
-                { name: "minecraft:planks", meta: 1, amount: plankAmount, chance: plankChance },
-                { name: "minecraft:log", meta: 1, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 10, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        mundaneLarch: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 0, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.0", meta: 0, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 11, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        bullPine: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 4, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.5", meta: 0, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 12, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        jungle: {
-            drops: defaultDrops.concat([
-                { name: "minecraft:planks", meta: 3, amount: plankAmount, chance: plankChance },
-                { name: "minecraft:log", meta: 3, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.0", meta: 15, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        teak: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 1, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.0", meta: 1, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 0, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        ipe: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 9, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.6", meta: 1, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 1, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        kapok: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 8, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.2", meta: 0, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 2, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        myrtleEbony: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 9, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.2", meta: 1, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 3, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        zebrawood: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 12, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.7", meta: 0, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 4, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        yellowMeranti: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 10, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.2", meta: 2, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 5, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        acacia: {
-            drops: defaultDrops.concat([
-                { name: "minecraft:planks", meta: 4, amount: plankAmount, chance: plankChance },
-                { name: "minecraft:log2", meta: 0, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 6, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        desertAcacia: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 2, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.0", meta: 2, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 7, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        padauk: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 10, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.6", meta: 2, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 8, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        balsa: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 11, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.2", meta: 3, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 9, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        cocobolo: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 11, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.6", meta: 3, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 10, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        wenge: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 5, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.1", meta: 1, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 11, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        grandidierBaobab: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 6, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.1", meta: 2, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 12, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        blueMahoe: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 0, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.4", meta: 0, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 13, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        whiteWillow: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 12, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.3", meta: 0, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 14, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        sipiri: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.0", meta: 14, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.3", meta: 2, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.1", meta: 15, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        papaya: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 3, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.4", meta: 3, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.2", meta: 0, amount: leafAmount, chance: leafChance },
-                { name: "forestry:fruits", meta: 6, amount: fruitAmount, chance: fruitChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        datePalm: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 2, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.4", meta: 2, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.2", meta: 1, amount: leafAmount, chance: leafChance },
-                { name: "forestry:fruits", meta: 5, amount: fruitAmount, chance: fruitChance }
-            ]),
-            growTime: defaultGrowTime
-        },
-        whitePoplar: {
-            drops: defaultDrops.concat([
-                { name: "forestry:planks.1", meta: 1, amount: plankAmount, chance: plankChance },
-                { name: "forestry:logs.4", meta: 1, amount: logAmount, chance: logChance },
-                { name: "forestry:leaves.decorative.2", meta: 2, amount: leafAmount, chance: leafChance }
-            ]),
-            growTime: defaultGrowTime
-        }
-    };
+    var blockedTrees = [
+        "forestry:giantSequoia",
+        "forestry:coastSequoia"
+    ];
 
-    Object.keys(forestryTrees).forEach(function(typeName) {
-        var data = forestryTrees[typeName];
+    TreeManager.treeRoot.getIndividualTemplates().forEach(function(tree) {
+        var label = tree.getDisplayName();
+        var id = tree.getGenome().getPrimary().getUnlocalizedName();
+        var modId = tree.getGenome().getPrimary().getModID();
 
-        var treeType = new TreeTypeForestry("forestry:" + typeName, saplingAmount, saplingChance);
-        if(data.growTime != null) {
-            treeType.setGrowTime(data.growTime);
+        if(modId == "forestry") {
+            id = id.replace('for.trees.species.', '');
+        } else if(modId == "extratrees") {
+            id = id.replace('extratrees.species.', '');
+            id = id.replace('.name', '');
         }
-        if(data.drops != null && data.drops.length > 0) {
-            data.drops.forEach(function(drop) {
-                treeType.addDrop(drop.name, drop.meta, drop.amount, drop.chance);
-            });
+
+        var fullId = modId + ":" + id;
+        if(blockedTrees.indexOf(fullId) != -1) {
+            return;
         }
+
+        var treeType = new TreeTypeForestry(fullId, saplingAmount, saplingChance);
+
+        var woodStack = tree.getGenome().getPrimary().getWoodProvider().getWoodStack();
+        woodStack.setCount(logAmount);
+        treeType.addDrop(woodStack, logChance);
+
+        var leafStack = tree.getGenome().getPrimary().getLeafProvider().getDecorativeLeaves();
+        leafStack.setCount(leafAmount);
+        treeType.addDrop(leafStack, leafChance);
+
+        tree.getProducts().entrySet().forEach(function(entry) {
+            var fruitStack = entry.getKey();
+            var chance = entry.getValue() * fruitChance;
+            fruitStack.setCount(fruitAmount);
+
+            treeType.addDrop(fruitStack, chance);
+        });
+
+        defaultDrops.forEach(function(drop) {
+            treeType.addDrop(drop.name, drop.meta, drop.amount, drop.chance);
+        });
 
         TreeTypeRegistry.registerTreeType(treeType);
     });
