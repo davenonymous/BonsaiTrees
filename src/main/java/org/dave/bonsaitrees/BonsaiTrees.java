b09d5f23ef3c0f0c330f6a4aaa5e3d669c915b7b
thraaawn <david.hilberath@gmx.de>
2018-05-31 23:01:25 +0200

    Bumped to 1.0.5

--- src/main/java/org/dave/bonsaitrees/BonsaiTrees.java
+++ src/main/java/org/dave/bonsaitrees/BonsaiTrees.java
@@ -22,7 +22,7 @@ import org.dave.bonsaitrees.utility.Logz;
 @Mod(modid = BonsaiTrees.MODID, version = BonsaiTrees.VERSION, acceptedMinecraftVersions = "[1.12,1.13)")
 public class BonsaiTrees {
     public static final String MODID = "bonsaitrees";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
 
     @Mod.Instance(BonsaiTrees.MODID)
     public static BonsaiTrees instance;
