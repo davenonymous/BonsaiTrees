b4db58ca1f0d264ae4f5ed1a56eae38c2ea767f0
thraaawn <david.hilberath@gmx.de>
2018-06-24 20:41:21 +0200

    Added support for SkyOrchards trees
    Since the trees in SkyOrchards are 100% dynamic this integration is
    not shipping any shapes by default. Creating the missing shapes can
    be done in bulk using the /bonsaitrees generateMissingShapes command.
    
    You don't have to create the shapes manually!

---
+++ src/main/java/org/dave/bonsaitrees/integration/mods/SkyOrchards.java
@@ -0,0 +1,51 @@
+package org.dave.bonsaitrees.integration.mods;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.dave.bonsaitrees.api.*;
+import org.dave.bonsaitrees.utility.Logz;
+import sky_orchards.blocks.BlockOreSapling;
+import sky_orchards.blocks.EnumWood;
+
+import java.util.Random;
+
+@BonsaiIntegration(mod = "sky_orchards")
+public class SkyOrchards implements IBonsaiIntegration {
+    @Override
+    public void registerTrees(ITreeTypeRegistry registry) {
+        for(EnumWood wood : EnumWood.values()) {
+            TreeTypeSimple treeType = new TreeTypeSimple("sky_orchard:" + wood.getName(), new ItemStack(wood.getSapling()));
+
+            treeType.addDrop(new ItemStack(wood.getSapling(), 1, 0), BonsaiDropChances.saplingChance);
+            treeType.addDrop(new ItemStack(Items.STICK, BonsaiDropChances.stickAmount), BonsaiDropChances.stickChance);
+            treeType.addDrop(new ItemStack(wood.getResin(), 1, 0), BonsaiDropChances.fruitChance);
+            treeType.addDrop(new ItemStack(wood.getLog(), 1, 0), BonsaiDropChances.logChance);
+            treeType.addDrop(new ItemStack(wood.getLeaves(), 1, 0), BonsaiDropChances.leafChance);
+
+            registry.registerTreeType(this, treeType);
+        }
+    }
+
+    @Override
+    public void generateTree(IBonsaiTreeType type, World world, BlockPos pos, Random rand) {
+        if(!(type.getExampleStack().getItem() instanceof ItemBlock)) {
+            Logz.warn("Sapling is no ItemBlock! This should not be happening!");
+            return;
+        }
+
+        Block sapling = ((ItemBlock) type.getExampleStack().getItem()).getBlock();
+        if(!(sapling instanceof BlockOreSapling)) {
+            Logz.info("Not an ore sapling");
+            return;
+        }
+
+        world.setBlockState(pos, sapling.getDefaultState());
+
+        BlockOreSapling oreSapling = (BlockOreSapling)sapling;
+        oreSapling.generateTree(world, pos, null, rand);
+    }
+}
