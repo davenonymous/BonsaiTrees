65102221f2ff5c5f645c74cac49fcfb11e4bdd24
thraaawn <david.hilberath@gmx.de>
2018-12-31 02:42:35 +0100

    Bumped to 1.1.2

--- src/main/java/org/dave/bonsaitrees/BonsaiTrees.java
+++ src/main/java/org/dave/bonsaitrees/BonsaiTrees.java
@@ -31,7 +31,7 @@ import org.dave.bonsaitrees.utility.Logz;
 )
 public class BonsaiTrees {
     public static final String MODID = "bonsaitrees";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
     public static final String GUI_FACTORY = "org.dave.bonsaitrees.misc.ConfigGuiFactory";
 
     @Mod.Instance(BonsaiTrees.MODID)
