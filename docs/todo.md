b4f2a5687896d7891a7705a61994978eb73bd8f5
davenonymous <david.hilberath@gmx.de>
2025-05-08 16:51:43 +0200

    Datapacks: Load by iterating the jar content
    We were previously iterating the modlist to see which packs we could
    load. This was the wrong way around. We should see which datapacks are
    available and register them regardless. As optional if no matching modid
    is found, required if there is a mod with the id loaded.

--- docs/todo.md
+++ docs/todo.md
@@ -1,11 +1,9 @@
 # Before release
 
-- Fix custom resource and data pack loading
-- Improve datapack loading (not mod-reliant, not always marked as required)
 - Fix item visibility in GUI slots
 - Finish documentation
 - Add The End trees (and those from mods extending The End)
-- GitHub Actions and Gradle integration fot auto-generating integrations
+- GitHub Actions and Gradle integration for auto-generating integrations
 - Try/catch soil and camouflage rendering and disable that particular block if it fails
 - Add a tick rate game server test, i.e. place n^2 pots with random saplings and check if the server
   can handle it
--- src/main/java/com/davenonymous/bonsaitrees/setup/ModDataPacks.java
+++ src/main/java/com/davenonymous/bonsaitrees/setup/ModDataPacks.java
@@ -15,71 +15,76 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.AddPackFindersEvent;
+import net.neoforged.neoforgespi.language.IModFileInfo;
 import net.neoforged.neoforgespi.locating.IModFile;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.Optional;
 
 @EventBusSubscriber(modid = BonsaiTrees.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class ModDataPacks {
-	private static final PackSelectionConfig RESOURCEPACK_SELECTION_CONFIG = new PackSelectionConfig(true, Pack.Position.BOTTOM, false);
-	private static final PackSelectionConfig DATAPACK_SELECTION_CONFIG = new PackSelectionConfig(true, Pack.Position.BOTTOM, false);
+	private static final PackSelectionConfig REQUIRED_SELECTION_CONFIG = new PackSelectionConfig(true, Pack.Position.BOTTOM, false);
+	private static final PackSelectionConfig OPTIONAL_SELECTION_CONFIG = new PackSelectionConfig(true, Pack.Position.BOTTOM, false);
 
 	@SubscribeEvent
 	public static void addPackFinder(AddPackFindersEvent event) {
+		IModFile bonsaiModFile = ModList.get().getModFileById(BonsaiTrees.MODID).getFile();
 		if(event.getPackType() == PackType.SERVER_DATA) {
-			IModFile bonsaiModFile = ModList.get().getModFileById(BonsaiTrees.MODID).getFile();
-
-			for(var modInfo : ModList.get().getMods()) {
-				String modId = modInfo.getModId();
-
-				try {
-					Path resourcePath = bonsaiModFile.findResource("datapacks", modId);
-
-					PackLocationInfo packLocationInfo = new PackLocationInfo(modId, Component.literal("Bonsais for: " + modInfo.getDisplayName()), PackSource.BUILT_IN,
+			Path datapacksPath = bonsaiModFile.findResource("datapacks");
+			try(var paths = Files.walk(datapacksPath)) {
+				for(var path : paths.filter(Files::isDirectory).sorted(Comparator.naturalOrder()).toList()) {
+					String modId = path.getFileName().toString();
+					IModFileInfo modFile = ModList.get().getModFileById(modId);
+					PackSelectionConfig selectionConfig = modFile == null ? OPTIONAL_SELECTION_CONFIG : REQUIRED_SELECTION_CONFIG;
+					PackLocationInfo packLocationInfo = new PackLocationInfo(modId, Component.literal("Bonsais: " + modId), PackSource.BUILT_IN,
 						Optional.empty()
 					);
-					PathPackResources packResources = new PathPackResources(packLocationInfo, resourcePath);
+					PathPackResources packResources = new PathPackResources(packLocationInfo, path);
 					PackMetadataSection packMeta = packResources.getMetadataSection(MetadataSectionType.fromCodec("pack", PackMetadataSection.CODEC));
 					if(packMeta == null) {
 						continue;
 					}
 
-					PathPackResources.PathResourcesSupplier pathResourcesSupplier = new PathPackResources.PathResourcesSupplier(resourcePath);
-					Pack pack = Pack.readMetaAndCreate(packLocationInfo, pathResourcesSupplier, event.getPackType(), DATAPACK_SELECTION_CONFIG);
+					PathPackResources.PathResourcesSupplier pathResourcesSupplier = new PathPackResources.PathResourcesSupplier(path);
+					Pack pack = Pack.readMetaAndCreate(packLocationInfo, pathResourcesSupplier, event.getPackType(), selectionConfig);
 					event.addRepositorySource(consumer -> {
 						consumer.accept(pack);
 					});
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
 
+				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
 			}
 
 		} else if(event.getPackType() == PackType.CLIENT_RESOURCES) {
-			IModFile bonsaiModFile = ModList.get().getModFileById(BonsaiTrees.MODID).getFile();
+			Path resourcePath = bonsaiModFile.findResource("resourcepacks");
+			try(var paths = Files.walk(resourcePath)) {
+				for(var path : paths.filter(Files::isDirectory).sorted(Comparator.naturalOrder()).toList()) {
+					String modId = path.getFileName().toString();
+					IModFileInfo modFile = ModList.get().getModFileById(modId);
+					PackSelectionConfig selectionConfig = modFile == null ? OPTIONAL_SELECTION_CONFIG : REQUIRED_SELECTION_CONFIG;
+					var packLocationInfo = new PackLocationInfo(modId, Component.literal("Bonsais: " + modId), PackSource.BUILT_IN, Optional.empty());
+					var packInfo = new PathPackResources(packLocationInfo, path);
+					try {
+						var packMeta = packInfo.getMetadataSection(MetadataSectionType.fromCodec("pack", PackMetadataSection.CODEC));
+						if(packMeta == null) {
+							continue;
+						}
 
-			for(var modInfo : ModList.get().getMods()) {
-				String modId = modInfo.getModId();
-				Path resourcePath = bonsaiModFile.findResource("resourcepacks", modId);
-
-				var packLocationInfo = new PackLocationInfo(modId, Component.literal("Bonsais for: " + modInfo.getDisplayName()), PackSource.BUILT_IN, Optional.empty());
-				var packInfo = new PathPackResources(packLocationInfo, resourcePath);
-				try {
-					var packMeta = packInfo.getMetadataSection(MetadataSectionType.fromCodec("pack", PackMetadataSection.CODEC));
-					if(packMeta == null) {
-						continue;
+						event.addRepositorySource(consumer -> {
+							consumer.accept(Pack.readMetaAndCreate(packLocationInfo,
+								new PathPackResources.PathResourcesSupplier(path), event.getPackType(), selectionConfig
+							));
+						});
+					} catch (IOException e) {
+						throw new RuntimeException(e);
 					}
-
-					event.addRepositorySource(consumer -> {
-						consumer.accept(Pack.readMetaAndCreate(packLocationInfo,
-							new PathPackResources.PathResourcesSupplier(resourcePath), event.getPackType(), RESOURCEPACK_SELECTION_CONFIG
-						));
-					});
-				} catch (IOException e) {
-					throw new RuntimeException(e);
 				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
 			}
 
 		}
