5f1d237aa7d618a6f64f52d99f7796307622119a
davenonymous <david.hilberath@gmx.de>
2025-06-12 16:22:20 +0200

    GUI: allow disabling of the progress arrow

--- src/main/java/com/davenonymous/bonsaitrees/lib/gui/widgets/WidgetProgressArrow.java
+++ src/main/java/com/davenonymous/bonsaitrees/lib/gui/widgets/WidgetProgressArrow.java
@@ -1,15 +1,27 @@
 package com.davenonymous.bonsaitrees.lib.gui.widgets;
 
 import com.davenonymous.bonsaitrees.lib.gui.GUIHelper;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 
 public class WidgetProgressArrow extends WidgetWithRangeValue<Double> {
+	private boolean disabled = false;
+
 	public WidgetProgressArrow() {
 		this.value = 0D;
 		this.setRange(0D, 100D);
 		this.setWidth(22);
 		this.setHeight(15);
+	}
+
+	public boolean disabled() {
+		return disabled;
+	}
+
+	public WidgetProgressArrow setDisabled(boolean disabled) {
+		this.disabled = disabled;
+		return this;
 	}
 
 	@Override
@@ -20,10 +32,17 @@ public class WidgetProgressArrow extends WidgetWithRangeValue<Double> {
 		int bgSpriteX = 42;
 		int progressSpriteX = 102;
 
-		//pGuiGraphics.blitSprite(GUI.tabIcons, spriteWidth, spriteHeight, bgSpriteX, spriteY, this.x, this.y, spriteWidth, spriteHeight);
-		pGuiGraphics.blit(GUIHelper.tabIcons, 0, 0, bgSpriteX, spriteY, spriteWidth, spriteHeight);
+		if(this.disabled) {
+			RenderSystem.setShaderColor(0.95f, 0f, 0f, 1.0f);
+			pGuiGraphics.blit(GUIHelper.tabIcons, 0, 0, bgSpriteX, spriteY, spriteWidth, spriteHeight);
+			RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+		} else {
+			pGuiGraphics.blit(GUIHelper.tabIcons, 0, 0, bgSpriteX, spriteY, spriteWidth, spriteHeight);
+		}
 
-		int progressWidth = (int) (spriteWidth * (this.value - this.rangeMin) / (this.rangeMax - this.rangeMin));
-		pGuiGraphics.blit(GUIHelper.tabIcons, 0, 0, progressSpriteX, spriteY, progressWidth, spriteHeight + 1);
+		if(!this.disabled) {
+			int progressWidth = (int) (spriteWidth * (this.value - this.rangeMin) / (this.rangeMax - this.rangeMin));
+			pGuiGraphics.blit(GUIHelper.tabIcons, 0, 0, progressSpriteX, spriteY, progressWidth, spriteHeight + 1);
+		}
 	}
 }
